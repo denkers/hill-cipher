aaf5945dd3e796785fada8811b355a183504d088
denkers <kyle.business@inbox.com>
2016-07-09 14:46:44 +1200

    Fixed getBigrams method to use correct offset in hillc.breaker.HillCipherBreaker

--- hillc-breaker/src/com/kyleruss/hillc/breaker/HillCipherBreaker.java
+++ hillc-breaker/src/com/kyleruss/hillc/breaker/HillCipherBreaker.java
@@ -25,9 +25,10 @@ public class HillCipherBreaker
     public static Entry<CStructure, CStructure> getAttemptRound(CStructure cipher, String knownPlaintext, int offset)
     {
         boolean startBigram         =   offset % 2 == 0;
-        int startIndex              =   startBigram? offset : offset + 1;
+        int startIndex              =   startBigram? 0 : 1;
+        int startOffset             =   startBigram? offset : offset + 1;
         CStructure guessStruc       =   getBigrams(knownPlaintext, startIndex);
-        CStructure cipherStruc      =   getBigrams(cipher.getText(), startIndex);
+        CStructure cipherStruc      =   getBigrams(cipher.getText(), startOffset);
         CStructure invCipherStruc   =   new CStructure(MatrixUtils.getInverse(cipherStruc.getMatrix()));
         CStructure decKey           =   new CStructure(MatrixUtils.multiplyDimMatrix(guessStruc.getMatrix(), invCipherStruc.getMatrix(), 255));
         CStructure decryptedCipher  =   HillCipher.decrypt(cipher, decKey);
@@ -37,10 +38,8 @@ public class HillCipherBreaker
     
     public static CStructure getBigrams(String text, int offset)
     {
-        boolean startBigram     =   offset % 2 == 0;
-        int startIndex          =   startBigram? offset : offset + 1;
-        CStructure bigramA      =   new CStructure(text.substring(startIndex, startIndex + 2));
-        CStructure bigramB      =   new CStructure(text.substring(startIndex + 2));
+        CStructure bigramA      =   new CStructure(text.substring(offset, offset + 2));
+        CStructure bigramB      =   new CStructure(text.substring(offset + 2));
         int[][] bigramMatrix    =   new int[2][2];
         
         bigramMatrix[0]         =   bigramA.getRow(0);
@@ -51,10 +50,11 @@ public class HillCipherBreaker
     
     public static void main(String[] args)
     {
-        String test =   "abcde";
-        int start   =   1;
-        String sub  =   test.substring(start, start + 2);
-        String sub2 =   test.substring(start + 2);
-        System.out.println("sub: " + sub + "\nsub2: " + sub2);
+        CStructure cipher   =   new CStructure("fupcmtgzkyukbqfjhuktzkkixtta");
+        String knownText    =   "ofthe";
+        List<Entry<CStructure, CStructure>> attempts    =   breakCipher(cipher, knownText);
+        
+        for(Entry<CStructure, CStructure> attempt : attempts)
+            System.out.println(attempt.getValue().getText());
     }
 }
