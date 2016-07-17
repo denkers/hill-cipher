6265571ecf663ea7937b61c1a76d986572e5d00a
denkers <kyle.business@inbox.com>
2016-07-17 16:52:52 +1200

    Added ChatMessageHandler

---
+++ hillc-chat/hillc-chat-server/src/com/kyleruss/hillc/server/message/ChatMessageHandler.java
@@ -0,0 +1,18 @@
+
+package com.kyleruss.hillc.server.message;
+
+import com.kyleruss.jsockchat.commons.io.MessageHandler;
+import com.kyleruss.jsockchat.commons.message.ActionHandler;
+import com.kyleruss.jsockchat.commons.message.MessageBean;
+import com.kyleruss.jsockchat.server.message.DefaultMessageHandler;
+
+public class ChatMessageHandler implements MessageHandler
+{
+
+    @Override
+    public ActionHandler getActionHandler(MessageBean mb)
+    {
+        MessageHandler defaultHandler   =   new DefaultMessageHandler();
+        return defaultHandler.getActionHandler(mb);
+    }
+}
