3a45212457c7c6ca16f0376235ae09eebdc66b4c
denkers <kyle.business@inbox.com>
2016-07-18 19:02:12 +1200

    Added client.Connfig

---
+++ hillc-chat/hillc-chat-client/src/com/kyleruss/hillc/client/Config.java
@@ -0,0 +1,11 @@
+package com.kyleruss.hillc.client;
+
+public class Config 
+{
+    //------------------------------------------
+    //  GUI CONFIG
+    //------------------------------------------
+    public static int WINDOW_HEIGHT     =   500;
+    public static int WINDOW_WIDTH      =   700;
+    public static String WINDOW_TITLE   =   "HillCipherChat - Kyle Russell 2016";
+}
