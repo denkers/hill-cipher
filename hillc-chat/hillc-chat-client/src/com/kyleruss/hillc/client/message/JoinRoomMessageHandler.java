b3c07f9f99c5f6718a54f16bbbba6bfc63bbb9aa
denkers <kyle.business@inbox.com>
2016-07-21 03:26:37 +1200

    Added client.message.JoinRoomMessageHandler

--- hillc-chat/hillc-chat-client/src/com/kyleruss/hillc/client/message/JoinRoomMessageHandler.java
+++ hillc-chat/hillc-chat-client/src/com/kyleruss/hillc/client/message/JoinRoomMessageHandler.java
@@ -1,20 +1,19 @@
-
 package com.kyleruss.hillc.client.message;
 
 import com.kyleruss.jsockchat.client.message.ClientMessageHandler;
 import com.kyleruss.jsockchat.commons.message.Message;
 
-public class BroadcastMessageHandler implements ClientMessageHandler
+public class JoinRoomMessageHandler implements ClientMessageHandler
 {
     @Override
     public void witnessAction(Message message) 
     {
-        System.out.println("[Broadcast witness action] " + message.getDescription());
+        System.out.println("[JoinRoom witness action] " + message.getDescription());
     }
 
     @Override
     public void performAction(Message message) 
     {
-        System.out.println("[Broadcast action] " + message.getDescription());
+        System.out.println("[JoinRoom action] " + message.getDescription());
     }
 }
