6153a80801e2794fc9e1d081a5ce7f043c0d6cfe
denkers <kyle.business@inbox.com>
2016-07-23 09:37:18 +1200

    Added javadocs to hillc.base.HillCipher

--- hillc-base/src/com/kyleruss/hillc/base/HillCipher.java
+++ hillc-base/src/com/kyleruss/hillc/base/HillCipher.java
@@ -8,11 +8,32 @@ package com.kyleruss.hillc.base;
 
 public class HillCipher 
 {
+    //---------------------------------------------
+    //  CHARACTER/STRUCTURE CONSTS
+    //---------------------------------------------
+    //Defines use of alphabetic only characters
     public static final int ALPHA_CHARS     =   26;
-    public static final int ALL_CHARS       =   255;
-    public static final int SMALL_V         =   2;
-    public static final int LARGE_V         =   3;
     
+    //Defines use of all ASCII characters
+    public static final int ALL_CHARS       =   255;
+    
+    //Represents a small vector size m = 2
+    public static final int SMALL_V         =   2;
+    
+    //Represents a larger vector size m = 3
+    public static final int LARGE_V         =   3;
+    //---------------------------------------------
+    
+    /**
+     * Encrypts the input plain text using the input key using the HillCipher algorithm
+     * Multiplies each row of the plaintext with key % @param mod
+     * Each ith row of the resulting cipher structure corresponds to the encrypted
+     * ith row of the input plain text structure 
+     * @param plainText A CStructure representing the plain text to encrypt
+     * @param key A CStructure representing the key used to encrypt the plain text
+     * @param mod The modulus used in operations. See ALPHA_CHARS, ALL_CHARS for options
+     * @return The encrypted cipher CStructure
+     */
     public static CStructure encrypt(CStructure plainText, CStructure key, int mod)
     {
         int size                =   key.getVectorSize();
@@ -29,6 +50,15 @@ public class HillCipher
         return new CStructure(cipherMatrix, mod, plainText.getVectorSize());
     }
     
+    /**
+     * Decryptes the input cipher with an existing input inverse key
+     * Builds the plain text back up  by multiplying each row of the 
+     * input cipher matrix with the inverse key % mod
+     * @param cipher The encrypted cipher CStructure
+     * @param invKey An inverted key CStructure (use decrypt if you don't have inv key)
+     * @param mod The modulus used in operations. See ALPHA_CHARS, ALL_CHARS for options
+     * @return The decrypted plain text CStructure 
+     */
     public static CStructure invKeyDecrypt(CStructure cipher, CStructure invKey, int mod)
     {
         String plainText    =   "";
@@ -45,6 +75,17 @@ public class HillCipher
         return new CStructure(plainText, mod, cipher.getVectorSize());
     }
     
+    /**
+     * Decryptes the input cipher with an existing input inverse key
+     * Builds the plain text back up  by multiplying each row of the 
+     * input cipher matrix with the inverse key % mod
+     * Finds the inverse key with MatrixUtils.getInverse and calls HillCipher.invKeyDecrpyt
+     * @see HillCipher.invKeyDecrypt
+     * @param cipher The encrypted cipher CStructure
+     * @param key A key CStructure (NOT the inverse key, @see HillCipher.invKeyDecrypt if you have inv key)
+     * @param mod The modulus used in operations. See ALPHA_CHARS, ALL_CHARS for options
+     * @return The decrypted plain text CStructure 
+     */
     public static CStructure decrypt(CStructure cipher, CStructure key, int mod)
     {
         CStructure invKey   =   new CStructure(MatrixUtils.getInverse(key.getMatrix(), mod), mod, cipher.getVectorSize());
